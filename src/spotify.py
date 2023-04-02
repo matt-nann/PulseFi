a71547c16480135274b8e0e28b7da0bbf8e931fd
Matt Nann <matpar481@gmail.com>
2023-04-02 12:17:12 -0400

    retrieving recently played music

---
+++ src/spotify.py
@@ -0,0 +1,12 @@
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client_id = os.environ.get('SPOTIFY_CLIENT_ID')
+client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
+
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
