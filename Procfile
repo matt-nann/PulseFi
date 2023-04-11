e39b60013b3c04bcbd30d122c4e091c6f2ee688b
Matt Nann <matpar481@gmail.com>
2023-04-11 10:45:19 -0400

    try this

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run:app
+web: gunicorn --pythonpath src run:app
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from src.flask.app import create_app
-from src.data.callbacks import FitbitAuth
 
 if __name__ == '__main__':
     app = create_app()
